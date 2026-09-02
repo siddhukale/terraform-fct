71bd85e76a3d2dfeb27f3cd5867aa9bca00798c4
siddheshkale <siddheshkale1212@gmail.com>
2026-09-02 16:57:19 +0530

    added desired value

--- DAY2/instance.tf
+++ DAY2/instance.tf
@@ -6,7 +6,7 @@ provider "aws" {
 resource "aws_instance" "fct-instance" {
   ami           = "ami-090d68841c2a28756"
   instance_type = "t3.micro"
-
+  count = 3
   tags = {
     Name = "first-instance"
   }
