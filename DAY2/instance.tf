933421a6c6cd9049b4f92cd9c33adcc6ef1ca2a5
siddheshkale <siddheshkale1212@gmail.com>
2026-09-02 23:15:42 +0530

    add the server name

--- DAY2/instance.tf
+++ DAY2/instance.tf
@@ -3,11 +3,20 @@ provider "aws" {
   
 }
 
-resource "aws_instance" "fct-instance" {
-  ami           = "ami-090d68841c2a28756"
-  instance_type = "t3.micro"
-  count = 3
-  tags = {
-    Name = "first-instance"
-  }
+# resource "aws_instance" "fct-instance" {
+#   ami           = "ami-090d68841c2a28756"
+#   instance_type = "t3.micro"
+#   count = 3
+#   tags = {
+#     Name = "first-instance"
+#   }
+# }
+
+resource "aws_instance" "fct-instance-2" {
+    for_each = toset(["jump server","application server","db server"])
+ instance_type = "t3.micro" 
+ ami = "ami-090d68841c2a28756"
+ tags = {
+   Name = each.key
+ }
 }
