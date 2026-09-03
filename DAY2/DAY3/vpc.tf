587ed103a76125cf123d623992bf8cbe8b27383b
siddheshkale <siddheshkale1212@gmail.com>
2026-09-03 19:38:33 +0530

    added new vpc

---
+++ DAY2/DAY3/vpc.tf
@@ -0,0 +1,8 @@
+resource "aws_vpc" "my-vpc" {
+    cidr_block = "10.0.0.0/16"
+
+    tags = {
+    Name = "my-1vpc"    
+    }
+  
+}
--- DAY2/instance.tf
+++ DAY2/instance.tf
@@ -21,12 +21,15 @@ provider "aws" {
 #  }
 # }
 
-resource "aws_instance" "fct-instance-2" {
- instance_type = "t3.micro" 
- count = 3
- ami = "ami-090d68841c2a28756"
- tags = {
-   Name = "web-server-${count.index}"
- }
-}
+##### It's a not runed on git and terraform server try this 
+# resource "aws_instance" "fct-instance-2" {
+#  instance_type = "t3.micro" 
+#  count = 3
+#  ami = "ami-090d68841c2a28756"
+#  key_name = "siddhesh-key"
+#  security_groups = ["sg-06d252d7082da7d52"]
+#  tags = {
+#    Name = "web-server${count.index+1}"
+#  }
+# }
 
