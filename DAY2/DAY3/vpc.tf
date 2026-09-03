886bafc06e1f472e1940c95e3083e870c2ba62b0
siddheshkale <siddheshkale1212@gmail.com>
2026-09-03 21:09:47 +0530

    again changes in file

--- DAY2/DAY3/vpc.tf
+++ DAY2/DAY3/vpc.tf
@@ -2,23 +2,23 @@ provider "aws" {
   
 }
 
-#### Create Vpc ###### 
+### Create Vpc ###### 
 
-# resource "aws_vpc" "my-vpc" {
-#     cidr_block = "10.0.0.0/16"
+resource "aws_vpc" "my_vpc" {
+    cidr_block = "10.0.0.0/16"
 
-#     tags = {
-#     Name = "my-1vpc"    
-#     }
+    tags = {
+    Name = "my-1vpc"    
+    }
   
-# }
+}
 
 
 ###### Create a  Subnet in that VPC which is Already Created##
 
 
 resource "aws_subnet" "new-subnet" {
-vpc_id =   "vpc-0c66533582604ff8c"
+vpc_id =   "aws_vpc.my_vpc.id"
 cidr_block = "10.0.0.0/20"
 
 tags = {
