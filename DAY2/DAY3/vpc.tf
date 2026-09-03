3c334a5721313816f318ecef7a7ffade28af63ae
siddheshkale <siddheshkale1212@gmail.com>
2026-09-03 21:36:44 +0530

    added igw

--- DAY2/DAY3/vpc.tf
+++ DAY2/DAY3/vpc.tf
@@ -14,6 +14,7 @@ resource "aws_vpc" "my_vpc" {
 }
 
 
+
 ###### Create a  Subnet in that VPC which is Already Created##
 
 
@@ -24,4 +25,24 @@ cidr_block = "10.0.0.0/20"
 tags = {
   Name = "My-Subnet"
 }
+}
+
+#### Create Second SubNet ###
+resource "aws_subnet" "new-subnet-1" {
+  vpc_id = aws_vpc.my_vpc.id
+  cidr_block = "10.0.1.0/20"
+
+  tags = {
+    Name = "My-subnet-2"
+  }
+}
+
+##### Create a internet gateway #####
+
+resource "aws_internet_gateway" "my_igw" {
+    vpc_id = aws_vpc.my_vpc.id
+    tags = {
+      Name = "my-internet-gateway"
+    }
+  
 }
