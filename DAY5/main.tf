1ed0f6e7718edaf01430f63bbcbd6825fad08a58
siddheshkale <siddheshkale1212@gmail.com>
2026-09-05 11:53:08 +0530

    added new ec2

---
+++ DAY5/main.tf
@@ -0,0 +1,16 @@
+
+provider "aws"  {
+    region = "var.region_name"
+  
+}
+
+resource "aws_instance" "my_instance" {
+  ami = "ami-090d68841c2a28756"
+  instance_type = "t2.micro"
+  key_name = "siddhesh-key"
+vpc_security_group_ids = [ "sg-04ac1d41063a71696" ]
+
+  tags ={
+    Name = "web-server"
+  } 
+}
---
+++ DAY5/variable.tf
@@ -0,0 +1,3 @@
+variable "region_name" {
+default = "ap-south-1b"
+}
