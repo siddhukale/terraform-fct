11b2fe6b9af78cf9ac2b263406b380d677f5d3fc
siddheshkale <siddheshkale1212@gmail.com>
2026-09-05 13:18:01 +0530

    added variable script with main.tf

--- DAY5/main.tf
+++ DAY5/main.tf
@@ -1,3 +1,4 @@
+## Create a EC2 instance using main.tf with varible. #######
 
 provider "aws"  {
     region = var.region_name
@@ -5,13 +6,22 @@ provider "aws"  {
 }
 
 resource "aws_instance" "my_instance" {
-  ami = "ami-090d68841c2a28756"
-  instance_type = "t3.micro"
-  key_name = "siddhesh-key"
-  subnet_id = "subnet-00ecb13ce28393ab9"
-vpc_security_group_ids = [ "sg-04ac1d41063a71696" ]
+ # ami = "ami-090d68841c2a28756"
+  ami = var.ami_image
+  
+  # instance_type = "t3.micro"
+  instance_type = var.instance_config
 
-  tags ={
+ # key_name = "siddhesh-key"
+ key_name = var.key_name
+
+  #subnet_id = "subnet-00ecb13ce28393ab9"
+  subnet_id = var.my_subnet
+
+  # vpc_security_group_ids = [ "sg-04ac1d41063a71696" ]
+  vpc_security_group_ids = [var.my_sg]
+
+  tags = {
     Name = "web-server"
   } 
 }
--- DAY5/variable.tf
+++ DAY5/variable.tf
@@ -1,3 +1,26 @@
+#### creat variable file 
+
 variable "region_name" {
 default = "ap-south-1"
+}
+variable "ami_image" {
+default = "ami-090d68841c2a28756"
+}
+
+variable "instance_config" {
+    default = "t3.micro"
+  
+}
+
+variable "my_sg" {
+  default = "sg-04ac1d41063a71696"
+}
+
+variable "key_name" {
+    default = "siddhesh-key"
+  
+}
+
+variable "my_subnet" {
+  default = "subnet-00ecb13ce28393ab9"
 }
