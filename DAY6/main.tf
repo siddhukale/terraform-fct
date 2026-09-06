631c815572c1b1be3ed0f3bfc8a38001cc816a2e
siddheshkale <siddheshkale1212@gmail.com>
2026-09-06 14:10:01 +0530

    added new script user data

---
+++ DAY6/main.tf
@@ -0,0 +1,24 @@
+provider "aws" {
+    region = var.region_name
+
+}
+
+resource "aws_instance" "new-instance" {
+  
+instance_type = var.instance_type
+ami = var.image_name
+key_name = var.key_name
+vpc_security_group_ids = var.sg
+
+user_data = <<-EOF
+            #!/bin/bash
+            yum install httpd -y
+            systemctl start httpd
+            systemctl enable httpd
+            echo "<h> This is USER-DATA Inside Terraform File </h>" > /var/www/html/index.html
+            EOF
+
+tags = {
+    Name = "User-Data"
+}
+}
---
+++ DAY6/variable.tf
@@ -0,0 +1,20 @@
+variable "region_name" {
+    default = "ap-south-1"
+  
+}
+
+variable "instance_type" {
+    default = "t3.micro"
+  
+}
+variable "key_name" {
+    default = "siddhesh-key"
+  
+}
+variable "image_name" {
+default =   "ami-090d68841c2a28756"
+}
+variable "sg" {
+    default = "sg-04ac1d41063a71696"
+  
+}
